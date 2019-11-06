b57b2eaabb2f263b24c0243e95c8f5a468b5ed4c
Gerald Sim <gerald@meappy.com>
2019-11-06 14:36:51 +1030

    update launch script

--- launch-web.sh
+++ launch-web.sh
@@ -2,7 +2,7 @@
 
 VPCSTACKNAME="$(grep ParamVpcStackName params/launch-web.json | awk '{print $5}' | sed 's/"//g')"
 
-aws --profile "${1}" cloudformation create-stack \
+aws --profile "${1:-default}" cloudformation create-stack \
     --stack-name "${VPCSTACKNAME}" \
     --parameters file://params/launch-web.json \
     --template-body file://launch-web.yaml 
